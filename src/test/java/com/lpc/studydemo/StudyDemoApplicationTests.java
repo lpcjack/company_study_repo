ab5ea04522efc5068ba319acd1434dea6f1d1c30
lpc <vbfgde546@163.com>
2025-03-17 13:28:41 +0800

    版本1.0.0

---
+++ src/test/java/com/lpc/studydemo/StudyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpc.studydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
